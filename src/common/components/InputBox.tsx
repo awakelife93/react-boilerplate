bdc9440fdf05817ae4a051c0fe3eeb4ca7a0314a
harry <awakelife93@gmail.com>
2021-06-09 16:26:53 +0900

    color 하드코딩 수정

--- src/common/components/InputBox.tsx
+++ src/common/components/InputBox.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { CommonComponentIE } from ".";
+import { CommonColor } from "../styles";
 
 interface InputBoxIE extends CommonComponentIE {}
 export const InputBox = styled.input`
   width: ${(props: InputBoxIE) => props["width"] ?? "300px"};
   height: ${(props: InputBoxIE) => props["height"] ?? "40px"};
   margin: ${(props: InputBoxIE) => props["margin"] ?? "0px"};
-  border: ${(props: InputBoxIE) => props["border"] ?? "1px solid #ced4da"};
+  border: ${(props: InputBoxIE) =>
+    props["border"] ?? `1px solid ${CommonColor.INPUT_COLOR}`};
   border-radius: ${(props: InputBoxIE) => props["border-radius"] ?? "0.25em"};
   margin-bottom: ${(props: InputBoxIE) => props["margin-bottom"] ?? "0px"};
 `;
--- src/common/styles/text.ts
+++ src/common/styles/text.ts
@@ -1,1 +1,2 @@
+// i18n vs object 고민해보기
 export {};
