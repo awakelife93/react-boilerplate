71665b5dca0295015c81a8cfe763e03246c5f194
harry <awakelife93@gmail.com>
2021-07-22 17:09:21 +0900

    removeUser -> tokenRemoveUser 수정

--- src/api/DeleteAPI/index.ts
+++ src/api/DeleteAPI/index.ts
@@ -2,7 +2,7 @@ import { deleteAPI } from "..";
 
 export const deleteUser = async () => {
   try {
-    let result: object = await deleteAPI("removeUser");
+    let result: object = await deleteAPI("tokenRemoveUser");
     return result;
   } catch (e) {
     console.log("===============> deleteUser Error", e);
