dab11e46933bfd53bb8ab645d74809e6a08aecaa
harry <awakelife93@gmail.com>
2022-03-20 14:17:38 +0900

    signUp endpoint signUp -> createUser로 변경

--- src/api/PostAPI/index.ts
+++ src/api/PostAPI/index.ts
@@ -35,7 +35,7 @@ export const signUp = async ({
   password: string;
 }): Promise<IUserInfo> => {
   try {
-    return await postAPI("signUp", {
+    return await postAPI("createUser", {
       email,
       name,
       password,
