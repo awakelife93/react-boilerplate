5d911a282aa37bcfdbec7f0e73c943fdce281730
harry <awakelife93@gmail.com>
2022-05-13 20:06:29 +0900

    주석 제거

--- src/common/interface.ts
+++ src/common/interface.ts
@@ -3,7 +3,5 @@ import React from "react";
 export interface ILayout {}
 
 export interface IComponent {
-  // * 단순 HTML을 Return 하는 경우를 대비하여 - ReactElement
-  // * 컴포넌트 객체를 던질 경우를 대비하여 - FC
   children?: React.ReactElement;
 }
